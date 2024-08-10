eab1a8b34b41b09d593949d024ccc04cca451805
KriStar88 <kristina_arapova@mail.ru>
2024-08-10 14:52:44 +0300

    Добавлена возможность выбора валюты

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ from tkinter import messagebox as mb
 from tkinter import ttk
 
 
-def exchange_crypto():
-    name_coin = combobox.get()
+def update_c_label(event):
+    code = currency_combobox.get()
+    name = currency[code]
+    c_label.config(text=name)
 
+
+def exchange_crypto():
+    name_coin = coin_combobox.get()
+    name_currency = currency_combobox.get()
 #    mb.showinfo("Проверка", name_coin)
-    if name_coin:
+    if name_coin and name_currency:
         try:
-            response = requests.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+            response = requests.get(f"https://api.coingecko.com/api/v3/coins/markets?vs_currency={name_currency}")
             response.raise_for_status()
             coin = response.json()
             for name in coin:
                 if name['name'] == name_coin:
                     exchange_coin = name["current_price"]
-                    mb.showinfo("Курс обмена", f"Курс: {name_coin}:  {exchange_coin} USD")
+                    mb.showinfo("Курс обмена", f"Курс: {name_coin}:  {exchange_coin} {name_currency}")
                     break
             else:
                 mb.showerror("Ошибка", "Криптовалюта не найдена")
@@ -31,7 +37,7 @@ def exchange_crypto():
 
 window = Tk()
 window.title("Курсы обмена криптовалют")
-window.geometry("360x180")
+window.geometry("360x250")
 
 Label(text="Выберите название криптовалюты").pack(padx=10, pady=10)
 
@@ -49,9 +55,29 @@ coins = [
     "Cardano",
     "TRON"
 ]
-combobox = ttk.Combobox(values=coins)
-combobox.pack(padx=10, pady=10)
 
+currency = {
+    'rub': "Российский рубль",
+    'eur': "Евро",
+    'jpy': "Японская йена",
+    'cny': "Китайский юань",
+    'krw': "Южнокорейская вона",
+    'aed': "Дирхам ОАЭ",
+    'cad': "Канадский доллар",
+    'usd': "Американский доллар",
+    'try': "Турецкая лира",
+    'ars': "Аргентинский песо"
+}
 
-Button(text="Получить курс обмена к доллару", command=exchange_crypto).pack(padx=10, pady=10)
+coin_combobox = ttk.Combobox(values=coins)
+coin_combobox.pack(padx=10, pady=10)
+
+currency_combobox = ttk.Combobox(values=list(currency.keys()))
+currency_combobox.pack(padx=10, pady=10)
+currency_combobox.bind("<<ComboboxSelected>>", update_c_label)
+
+c_label = ttk.Label()
+c_label.pack(padx=10, pady=10)
+
+Button(text="Получить курс обмена ", command=exchange_crypto).pack(padx=10, pady=10)
 window.mainloop()
