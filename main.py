b4e7315ec2dc44099bcaf8b5777a9b6610103b73
KriStar88 <kristina_arapova@mail.ru>
2024-08-10 10:02:06 +0300

    Создали главное окно программы

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+import json
+from tkinter import *
+from tkinter import messagebox as mb
+
+
+window = Tk()
+window.title("Курсы обмена криптовалют")
+window.geometry("360x80")
+
+Label(text="Введите название криптовалюты").pack(padx=10, pady=10)
+
+entry = Entry()
+entry.pack(padx=10, pady=10)
+
+Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10, pady=10)
+window.mainloop()
