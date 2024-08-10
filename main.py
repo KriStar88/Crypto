43338071dc3b6c622e6b2c1a333e9838281a022f
KriStar88 <kristina_arapova@mail.ru>
2024-08-10 13:26:37 +0300

    название валюты не вводится, а выбирается из списка

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import requests
 import json
 from tkinter import *
 from tkinter import messagebox as mb
+from tkinter import ttk
 
 
 def exchange_crypto():
-    name_coin = entry.get()
+    name_coin = combobox.get()
+
 #    mb.showinfo("Проверка", name_coin)
     if name_coin:
         try:
@@ -13,11 +15,10 @@ def exchange_crypto():
             response.raise_for_status()
             coin = response.json()
             for name in coin:
-                if name['id'] == name_coin:
+                if name['name'] == name_coin:
                     exchange_coin = name["current_price"]
                     mb.showinfo("Курс обмена", f"Курс: {name_coin}:  {exchange_coin} USD")
-                else:
-                    mb.showerror("Ошибка", "Криптовалюта не найдена")
+
         except Exception as e:
             mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
 
@@ -32,10 +33,25 @@ window = Tk()
 window.title("Курсы обмена криптовалют")
 window.geometry("360x180")
 
-Label(text="Введите название криптовалюты").pack(padx=10, pady=10)
+Label(text="Выберите название криптовалюты").pack(padx=10, pady=10)
 
-entry = Entry()
-entry.pack()
+coins = [
+    "Bitcoin",
+    "Ethereum",
+    "Tether",
+    "BNB",
+    "Solana",
+    "USDC",
+    "XRP",
+    "Lido Staked Ether",
+    "Toncoin",
+    "Dogecoin",
+    "Cardano",
+    "TRON"
+]
+combobox = ttk.Combobox(values=coins)
+combobox.pack(padx=10, pady=10)
+
 
 
 Button(text="Получить курс обмена к доллару", command=exchange_crypto).pack(padx=10, pady=10)
